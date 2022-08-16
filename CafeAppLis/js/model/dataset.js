c0b278fa88f8d2dfd5071d8537c4f0ba39913773
Joálisson <clementejoalisson923@gmail.com>
2022-08-16 00:42:40 -0300

    primeiro commit

---
+++ CafeAppLis/js/model/dataset.js
@@ -0,0 +1,41 @@
+/**
+* Foi criado uma constante que são guardadas as informações em um JSON do javascript com propriedades id, nome e descrição.
+* As informações são iniciadas e executadas através da forma dinâmica com o preenchimento do modal do modal.
+*/
+
+const dataset =  [
+    {
+      identificação : 1 ,
+      nome : 'Hambúrguer' ,
+      imagem : 'imagens/hamburguer.jpg' ,
+      descrição :
+        'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres de carne, geralmente carne bovina, dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado' ,
+    } ,
+    {
+      identificação : 2 ,
+      nome : 'Sanduíche' ,
+      imagem : 'imagens/sanduiche.jpg' ,
+      descrição :
+        'O sanduíche de sanduíche (do sanduíche), também popularmente chamado de sanduíche de sanduíche (português brasileiro) ou sandes português europeu), é tipo de alimento que consiste em duass de um sanduíche inteiro, entre as quais é um pão de carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como refeição rápida, durante o almoço ou o jantar.' ,
+    } ,
+    {
+      ID : 3 ,
+      nome : 'Café' ,
+      imagem : 'imagens/café.jpg' ,
+      descrição :
+        'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido quente, mas também pode ser consumido congelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 80 a.' ,
+    } ,
+    {
+      ID : 4 ,
+      nome : 'Suco' ,
+      imagem : 'imagens/suco.jpg' ,
+      descrição :
+        'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser pelo processamento de outras partes dos vegetais, como folhas, talos ou raízes.' ,
+    } ,
+  ] ;
+  
+  
+  /**
+  * Exportação padrão do conjunto de dados do módulo.
+  */
+   export default dataset;
